d1117aed059c1d2c943a7328ecdaf3fa34cc605c
AhmedCode99 <144271024+AhmedCode99@users.noreply.github.com>
2024-07-22 20:47:08 -0400

    Update two_threads.py
    fixed file locking TypeError

--- two_threads.py
+++ two_threads.py
@@ -41,7 +41,8 @@ def save_location(file_path, batch_size=1000):
     if frame_count % batch_size != 0:
         raise ValueError("Frame count must be a multiple of batch size")
     batch = []
-    np.save(file_path, [[0, 0]])  # Create a file top save locations in
+    np.save(file_path, [[0, 0]])  # Create a file to save locations in
+    i = 1
     while True:
         loc = location_queue.get()
         if loc is None:
@@ -51,24 +52,26 @@ def save_location(file_path, batch_size=1000):
             loc_batch = np.array(batch)
             with open(file_path, 'r+b') as f:
                 fcntl.flock(f, fcntl.LOCK_EX)
-                location_list = np.load(f, mmap_mode='r+')
+                f.close()  # Close the file handle before loading with mmap_mode
+                location_list = np.load(file_path, mmap_mode='r+')
                 location_list = np.concatenate((location_list, loc_batch), axis=0)
-                f.seek(0)
-                np.save(f, location_list)
-                fcntl.flock(f, fcntl.LOCK_UN)
+                with open(file_path, 'wb') as f:
+                    np.save(f, location_list)
+                    fcntl.flock(f, fcntl.LOCK_UN)
             batch = []
-            print('batch saved')
+            print('batch {} saved'.format(i))
+            i = i + 1
         location_queue.task_done()
 
 # Load template
 template, h, w = load_template('template.npy')
 
 # Number of frames to process
-frame_count = 5000
+frame_count = 1500000
 
 # Create and start threads
 find_thread = threading.Thread(target=find_location, args=(template, frame_count))
-save_thread = threading.Thread(target=save_location, args=('location_test.npy', 1000))
+save_thread = threading.Thread(target=save_location, args=('location_test.npy', 150000))
 
 find_thread.start()
 save_thread.start()
