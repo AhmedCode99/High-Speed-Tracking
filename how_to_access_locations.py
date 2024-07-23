5c7418e6a30021652a85592a3cfee1aa1c25cf26
AhmedCode99 <144271024+AhmedCode99@users.noreply.github.com>
2024-07-23 14:31:41 -0400

    Update how_to_access_locations.py

--- how_to_access_locations.py
+++ how_to_access_locations.py
@@ -1,12 +1,6 @@
 import numpy as np
 import fcntl
-
-def read_locations(file_path):
-    with open(file_path, 'rb') as f:
-        fcntl.flock(f, fcntl.LOCK_SH)
-        location_list = np.load(f, mmap_mode='r')
-        fcntl.flock(f, fcntl.LOCK_UN)
-    return location_list
+from utility_functions import read_locations
 
 # Read the locations
 file_path = 'location_test.npy'
