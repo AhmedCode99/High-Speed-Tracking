ff2c6085494660d6364a28fdab9d12acf6b0dcb3
AhmedCode99 <144271024+AhmedCode99@users.noreply.github.com>
2024-07-22 21:14:58 -0400

    Update and rename two_threads.py to three_threads.py
    Thread for each of capturing frames, finding location, and saving location. Furthermore, file locking is utilized to be able to safely read the location while they are still being collected.

--- three_threads.py
+++ three_threads.py
@@ -15,10 +15,11 @@ camera.f.OffsetY.value = 231  # Set OffsetY
 camera.f.OffsetX.value = 304  # Set OffsetX
 camera.f.Gain.value = 1  # Set Gain
 camera.f.AcquisitionFrameRateEnable.value = True
-camera.f.AcquisitionFrameRate.value = 8000  # fps
+camera.f.AcquisitionFrameRate.value = 5000  # fps
 print("camera ok")
 
-# Queue to hold locations
+# Queues to hold frames and locations
+frame_queue = queue.Queue()
 location_queue = queue.Queue()
 
 # Function to load template
@@ -27,10 +28,19 @@ def load_template(file_path):
     h, w = template.shape
     return template, h, w
 
-# Function to find location
-def find_location(template, frame_count):
+# Function to capture frames
+def capture_frames(frame_count):
     for _ in range(frame_count):
         frame = camera.GetImage().GetNPArray()
+        frame_queue.put(frame)
+    frame_queue.put(None)  # Signal to find thread that capturing is done
+
+# Function to find location
+def find_location(template, frame_count):
+    while True:
+        frame = frame_queue.get()
+        if frame is None:
+            break
         result = cv2.matchTemplate(frame, template, cv2.TM_SQDIFF)
         min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
         location_queue.put(min_loc)
@@ -70,13 +80,16 @@ template, h, w = load_template('template.npy')
 frame_count = 1500000
 
 # Create and start threads
+capture_thread = threading.Thread(target=capture_frames, args=(frame_count,))
 find_thread = threading.Thread(target=find_location, args=(template, frame_count))
 save_thread = threading.Thread(target=save_location, args=('location_test.npy', 150000))
 
+capture_thread.start()
 find_thread.start()
 save_thread.start()
 
 # Wait for threads to finish
+capture_thread.join()
 find_thread.join()
 save_thread.join()
 
