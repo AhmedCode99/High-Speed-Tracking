bc05a43db321b0da9304e22084d213f66c4c0740
AhmedCode99 <144271024+AhmedCode99@users.noreply.github.com>
2024-07-23 12:02:47 -0400

    Update utility_functions.py
    Added comments

--- utility_functions.py
+++ utility_functions.py
@@ -14,12 +14,12 @@ def capture_frames(frame_count):
 # Function to find location
 def find_location(template, frame_count):
     while True:
-        frame = frame_queue.get()
-        if frame is None:
+        frame = frame_queue.get() # Get frame from queue
+        if frame is None: # Condition to exit the loop
             break
-        result = cv2.matchTemplate(frame, template, cv2.TM_SQDIFF)
-        min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
-        location_queue.put(min_loc)
+        result = cv2.matchTemplate(frame, template, cv2.TM_SQDIFF) # "Convolution"
+        min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result) # Find Location
+        location_queue.put(min_loc) # Add location to queue
     location_queue.put(None)  # Signal to save thread that processing is done
 
 # Function to save location in batches
@@ -28,23 +28,21 @@ def save_location(file_path, batch_size=1000):
         raise ValueError("Frame count must be a multiple of batch size")
     batch = []
     np.save(file_path, [[0, 0]])  # Create a file to save locations in
-    i = 1
+    
     while True:
-        loc = location_queue.get()
-        if loc is None:
+        loc = location_queue.get() # Get location from queue
+        if loc is None: # Condition to exit the loop
             break
         batch.append(loc)
         if len(batch) >= batch_size:
             loc_batch = np.array(batch)
             with open(file_path, 'r+b') as f:
-                fcntl.flock(f, fcntl.LOCK_EX)
+                fcntl.flock(f, fcntl.LOCK_EX) # Lock file
                 f.close()  # Close the file handle before loading with mmap_mode
                 location_list = np.load(file_path, mmap_mode='r+')
                 location_list = np.concatenate((location_list, loc_batch), axis=0)
                 with open(file_path, 'wb') as f:
                     np.save(f, location_list)
-                    fcntl.flock(f, fcntl.LOCK_UN)
+                    fcntl.flock(f, fcntl.LOCK_UN) # Unlock file
             batch = []
-            print('batch {} saved'.format(i))
-            i = i + 1
         location_queue.task_done()
