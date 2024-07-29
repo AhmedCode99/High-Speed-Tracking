5fbcc3e9ee92d1ae4f117a036d2a499783b020c8
AhmedCode99 <144271024+AhmedCode99@users.noreply.github.com>
2024-07-29 15:06:05 -0400

    Update and rename utility_functions.py to two_threads.py
    two threads only

--- two_threads.py
+++ two_threads.py
@@ -4,7 +4,34 @@ import neoapi
 import threading
 import queue
 import fcntl
+from npy_append_array import NpyAppendArray
+import psutil
+import os
+import time
 
+# Camera Settings
+camera = neoapi.Cam()  # Create camera object
+camera.Connect('700006383766')  # Connect to camera
+camera.f.ExposureTime.Set(20)  # Set exposure time
+camera.f.Width.value = 48  # Set width
+camera.f.Height.value = 48  # Set height
+camera.f.OffsetY.value = 132  # Set OffsetY
+camera.f.OffsetX.value = 512  # Set OffsetX
+camera.f.Gain.value = 1  # Set Gain
+camera.f.AcquisitionFrameRateEnable.value = True
+camera.f.AcquisitionFrameRate.value = 8000  # fps
+print("camera ok")
+
+# Queues to hold frames and locations
+
+location_queue = queue.Queue()
+memory_list = []
+
+def monitor_memory():
+    pid = os.getpid()
+    process = psutil.Process(pid)
+    mem_info = process.memory_info()
+    return mem_info.rss / (1024*1024)
 
 def load_template(file_path):
     """
@@ -20,7 +47,7 @@ def load_template(file_path):
     h, w = template.shape
     return template, h, w
 
-def capture_frames(frame_count):
+def capture_frames(frame_count, template):
     """
     Capture frames from the camera and put them into a queue.
     
@@ -29,43 +56,33 @@ def capture_frames(frame_count):
     """
     for _ in range(frame_count):
         frame = camera.GetImage().GetNPArray()  # Get Image as np.array
-        frame_queue.put(frame)  # Save it in Queue
-    frame_queue.put(None)  # Signal to the find thread that capturing is done
-
-def find_location(template, frame_count):
-    """
-    Find the location of the template in each frame and put the locations into a queue.
-    
-    Args:
-        template (numpy array): The template to match in frames.
-        frame_count (int): Number of frames to process.
-    """
-    while True:
-        frame = frame_queue.get()  # Get frame from queue
-        if frame is None:  # Condition to exit the loop
-            break
         result = cv2.matchTemplate(frame, template, cv2.TM_SQDIFF)  # Perform template matching
         min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)  # Find location
         location_queue.put(min_loc)  # Add location to queue
-    location_queue.put(None)  # Signal to the save thread that processing is done
+        del frame, result
+    location_queue.put(None)  # Signal to the find thread that capturing is done
 
-def save_location(file_path, batch_size=1000):
+
+
+def save_location(file_path, frame_count, batch_size=1000):
     """
-    Save locations in batches to a file.
+    Save locations in batches to a file using NpyAppendArray.
     
     Args:
         file_path (str): Path to the file where locations will be saved.
         batch_size (int): Number of locations per batch.
     
     Raises:
-        ValueError: If frame_count is not a multiple of batch_size.
+        ValueError: If frame_count is not a multiple of batch size.
     """
     if frame_count % batch_size != 0:
         raise ValueError("Frame count must be a multiple of batch size")
     
     batch = []
     np.save(file_path, [[0, 0]])  # Create a file to save locations in
-    
+
+    npaa = NpyAppendArray(file_path)
+    i = 1
     while True:
         loc = location_queue.get()  # Get location from queue
         if loc is None:  # Condition to exit the loop
@@ -74,15 +91,17 @@ def save_location(file_path, batch_size=1000):
         if len(batch) >= batch_size:
             loc_batch = np.array(batch)
             with open(file_path, 'r+b') as f:
-                fcntl.flock(f, fcntl.LOCK_EX)  # Lock file
-                f.close()  # Close the file handle before loading with mmap_mode
-                location_list = np.load(file_path, mmap_mode='r+')
-                location_list = np.concatenate((location_list, loc_batch), axis=0)
-                with open(file_path, 'wb') as f:
-                    np.save(f, location_list)
-                    fcntl.flock(f, fcntl.LOCK_UN)  # Unlock file
+                fcntl.flock(f, fcntl.LOCK_EX)  # Lock file exclusively
+                npaa.append(loc_batch)  # Append batch to file
+                fcntl.flock(f, fcntl.LOCK_UN)  # Unlock file
             batch = []
+            print('Batch {} Saved'.format(i))
+            i += 1
+            rss_memory = monitor_memory()
+            memory_list.append(rss_memory)
+            print('RSS Used = {} MB'.format(rss_memory))
         location_queue.task_done()
+
 
 def read_locations(file_path):
     """
@@ -101,7 +120,43 @@ def read_locations(file_path):
 
     """
     with open(file_path, 'rb') as f:  # Open File
-        fcntl.flock(f, fcntl.LOCK_SH)  # Lock file
+        fcntl.flock(f, fcntl.LOCK_EX)  # Lock file
         location_list = np.load(f, mmap_mode='r')  # Read data in file
         fcntl.flock(f, fcntl.LOCK_UN)  # Unlock file
     return location_list
+
+
+# Load template
+template, h, w = load_template('template.npy')
+
+# Number of frames to process
+frame_count = 30000000
+batch_size = 300000 #150000
+
+# File Path for saving locations
+file_path = 'location_test.npy'
+
+# Create and start threads
+capture_thread = threading.Thread(target=capture_frames, args=(frame_count,template))
+
+save_thread = threading.Thread(target=save_location, args=(file_path, frame_count, batch_size))
+
+capture_thread.start()
+
+save_thread.start()
+
+# Wait for threads to finish processing their queues
+
+location_queue.join()
+
+# Wait for threads to finish
+capture_thread.join()
+
+save_thread.join()
+
+# Save Memory Usage
+np.save('memory_usage.npy', memory_list)
+
+# Disconnect the camera
+camera.Disconnect()
+print("camera disconnected")
