b642412c750378d42e4cbca94d7bfabf0f933254
AhmedCode99 <144271024+AhmedCode99@users.noreply.github.com>
2024-07-29 15:46:12 -0400

    Update how_to_access_locations.py

--- how_to_access_locations.py
+++ how_to_access_locations.py
@@ -1,6 +1,28 @@
 import numpy as np
 import fcntl
-from utility_functions import read_locations
+
+def read_locations(file_path):
+    """
+    Reads a numpy array of locations from a specified file.
+
+    This function opens a file in binary read mode, applies a shared lock to ensure 
+    safe reading in a multi-process environment, loads the numpy array from the file 
+    using memory mapping for efficient reading, and then unlocks the file before returning 
+    the array.
+
+    Parameters:
+    file_path (str): The path to the file containing the numpy array of locations.
+
+    Returns:
+    numpy.ndarray: A numpy array containing the locations read from the file.
+
+    """
+    with open(file_path, 'rb') as f:  # Open File
+        fcntl.flock(f, fcntl.LOCK_EX)  # Lock file
+        location_list = np.load(f, mmap_mode='r')  # Read data in file
+        fcntl.flock(f, fcntl.LOCK_UN)  # Unlock file
+    return location_list
+
 
 # Read the locations
 file_path = 'location_test.npy'
